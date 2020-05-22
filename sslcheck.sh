9a2ff1a0d298424c3991b5ac175c51de08026752
jackey <jackey525@gmail.com>
2020-05-22 16:04:26 +0800

    Update sslcheck.sh

--- sslcheck.sh
+++ sslcheck.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-    gracedays=14
-    date=`echo |openssl x509 -enddate -noout -in /etc/ssl/certs/cdn-server.crt | sed -e 's#notAfter=##'`
-    ssldate=`date -d "${data}" '+%s'`
+    gracedays=30
+    date1=`echo |openssl x509 -enddate -noout -in /etc/ssl/certs/server.cer | sed -e 's#notAfter=##'`
+    ssldate=$(date -d "${date1}" +%s)
     nowdate=`date '+%s'`
     diff="$((${ssldate}-${nowdate}))"
-
+    #echo $diff
     if test "${diff}" -lt "$((${gracedays}*24*3600))";
     then
         if test "${diff}" -lt "0";
@@ -14,8 +13,10 @@
             echo $msg
             curl -G --data-urlencode "chat_id=-385404270" --data-urlencode "text=The certificate  has already expired" https://api.telegram.org/botxxx__your token__xxxx/sendMessage
         else
-            msg="The certificate for www.cdn-news.org  will expire in $((${diff}/3600/24)) days."
+            msg="The certificate for cdn-news.org  will expire in $((${diff}/3600/24)) days."
             echo $msg
             curl -G --data-urlencode "chat_id=-385404270" --data-urlencode "text=The certificate will expire in $((${diff}/3600/24)) days." https://api.telegram.org/botxxx__your token__xxxx/sendMessage
         fi
     fi
+
+    
